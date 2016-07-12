be518a8487132c72fe3d439a0f607e302339c24e
Andrey Holovko <holovko@gmail.com>
2016-07-12 23:18:40 +0300

    Splash screen

---
+++ Capstone2/Capstone2/app/src/main/java/com/holovko/kyivmommap/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.holovko.kyivmommap;
+
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+     /*   int SPLASH_DISPLAY_LENGTH = 1500;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        },SPLASH_DISPLAY_LENGTH);*/
+    }
+}
